eaabc76c89c25e269b1360d9c1e48ba0671a07fd
Mohd-Imad <mohdimd123@gmail.com>
2022-12-21 15:46:14 +0530

    ok

---
+++ rubixe-assignment/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <Link to='/'><img src="" alt="" /></Link>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
