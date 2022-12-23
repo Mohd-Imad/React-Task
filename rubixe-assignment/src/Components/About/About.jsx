4c08034ba92d2c0c7154eff4846bda21d4aacf8a
Mohd-Imad <mohdimd123@gmail.com>
2022-12-23 09:17:46 +0530

    ok

--- rubixe-assignment/src/Components/About/About.jsx
+++ rubixe-assignment/src/Components/About/About.jsx
@@ -8,17 +8,20 @@ const About = () => {
       <div className="row">
         <section className="columns">
           <div className="col-left">
-            <h2>WHO WE ARE</h2>
-            <p>Rubixe™ is a technology company specializing in disruptive technologies - Artificial Intelligence (AI), Machine Learning, Robotic Process Automation (RPA), BlockChain and Internet of Things (IoT)</p>
+            <h1>WHO WE ARE</h1><br />
+            <p>Rubixe™ is a technology company specializing in disruptive technologies - Artificial Intelligence (AI), Machine Learning, Robotic Process Automation (RPA), BlockChain and Internet of Things (IoT)</p><br />
             <p>Rubixe™ mission is to enable businesses to leverage the full potential of disruptive technologies and stay competitive in the market, turning complex challenges into solutions, providing a strategic competitive advantage that one more efficient, effective and predictable.</p>
           </div>
           <div className="col-right">
-            <img src={AboutImg} alt="about-img" />
+            <div className="line"></div>
+            <img className='about-img' src={AboutImg} alt="about-img" />
+            <div className='green-rect'></div>
           </div>
         </section>
       </div>
     </>
   )
 }
+
 
 export default About
--- rubixe-assignment/src/Components/History/History.jsx
+++ rubixe-assignment/src/Components/History/History.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './History.css'
 
 const History = () => {
-  return (
-    <>
-      <h1>History</h1>
-    </>
-  )
+  return <>
+  <div className='history'>
+    <h1>WHERE WE STARTED</h1>
+    <p>We started in 2015, With a passionate team who want to bring game-changing solutions through disrupting technologies. We expertise in
+delivering enterprise-level solutions in the field of Artificial Intelligence (AI), Cyber Security, Robotic Process Automation (RPA), Internet of Things
+(IoT), and BlockChain technology.</p>
+  </div>
+  </>
 }
 
 export default History
